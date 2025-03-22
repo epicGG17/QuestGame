05ae2e602b3d205dae0ff5a698f54d2202da29aa
epicGG17 <47368424+epicGG17@users.noreply.github.com>
2025-03-22 13:33:42 -0700

    Changes sence and add lvls

---
+++ forest.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var inoutRef = $inOut
+	inoutRef.putNewArea("res://level_1/room_1.tscn")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
--- level_1/room_1.gd
+++ level_1/room_1.gd
@@ -3,16 +3,11 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect("body_entered", caveLevel)
+	var inoutRef = $inOut
+	inoutRef.putNewArea("res://forest.tscn")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-func caveLevel(body: Node3D):
-	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://level_1/room_6.tscn")
-
-
-
